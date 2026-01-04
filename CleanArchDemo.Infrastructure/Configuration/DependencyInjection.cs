54bf4210c1144fe765d92e7060dfe928f4f64faa
Hassan Olfat <HassanOlfat@users.noreply.github.com>
2026-01-04 14:14:07 +0330

    add infra into persentaion

---
+++ CleanArchDemo.Infrastructure/Configuration/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using CleanArchDemo.Application.Interfaces;
+using CleanArchDemo.Infrastructure.InMemory;
+
+namespace CleanArchDemo.Infrastructure.Configuration;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddScoped<ICustomerRepository, InMemoryCustomerRepository>();
+        services.AddScoped<IProductRepository, InMemoryProductRepository>();
+        services.AddScoped<IOrderRepository, InMemoryOrderRepository>();
+
+        return services;
+    }
+}
--- CleanArchDemo.Presentation/Program.cs
+++ CleanArchDemo.Presentation/Program.cs
@@ -1,7 +1,10 @@
+using CleanArchDemo.Infrastructure.Configuration;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddInfrastructure();
+
+builder.Services.AddControllers();
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
